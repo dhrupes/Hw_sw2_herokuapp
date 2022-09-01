c045160ed58fccf224f5f7872ad8c5f2e2f54a1b
dhrupes <dhrupestcb@gmail.com>
2022-09-01 17:09:40 +0100

    updated

--- src/testsuits/LoginTest.java
+++ src/testsuits/LoginTest.java
@@ -7,8 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import utilities.Utility;
 
-public class LoginTest extends BaseTest {
+import javax.print.DocFlavor;
+
+public class LoginTest extends Utility {
 
 /* 3. Write down the following test into ‘LoginTest’ class
 1. userSholdLoginSuccessfullyWithValidCredentials
@@ -29,16 +32,22 @@ public class LoginTest extends BaseTest {
     @Test
 
     public void userSholdLoginSuccessfullyWithValidCredentials() {
-        WebElement clickUsername = driver.findElement(By.xpath("//input[@name='username']"));
-        clickUsername.sendKeys("tomsmith");
-        WebElement clickPassword = driver.findElement(By.xpath("//input[@name='password']"));
-        clickPassword.sendKeys("SuperSecretPassword!");
-        WebElement clickLogin = driver.findElement(By.xpath("//button[@type='submit']"));
-        clickLogin.click();
-        WebElement verify = driver.findElement(By.xpath("//h4[@class='subheader']"));
+        sendKeysToElement(By.xpath("//input[@name='username']"),"tomsmith");
+        //WebElement clickUsername = driver.findElement(By.xpath("//input[@name='username']"));
+        //clickUsername.sendKeys("tomsmith");
+        sendKeysToElement(By.xpath("//input[@name='password']"),"SuperSecretPassword!");
+       // WebElement clickPassword = driver.findElement(By.xpath("//input[@name='password']"));
+       // clickPassword.sendKeys("SuperSecretPassword!");
+        clickOnElement(By.xpath("//button[@type='submit']"));
+       // WebElement clickLogin = driver.findElement(By.xpath("//button[@type='submit']"));
+        //clickLogin.click();
+        String actualMess= getTextFromElement(By.xpath("//h4[@class='subheader']"));
+        String expectedMess = "Welcome to the Secure Area. When you are done click logout below.";
+        Assert.assertEquals(expectedMess,actualMess);
+       /* WebElement verify = driver.findElement(By.xpath("//h4[@class='subheader']"));
         String actualMess = verify.getText();
         String expectedMess = "Welcome to the Secure Area. When you are done click logout below.";
-        Assert.assertEquals(expectedMess, actualMess);
+        Assert.assertEquals(expectedMess, actualMess); */
     }
     /* 2. verifyTheUsernameErrorMessage
      * Enter “tomsmith1” username
@@ -48,16 +57,23 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void verifyTheUsernameErrorMessage() {
-        WebElement clickUsername = driver.findElement(By.xpath("//input[@name='username']"));
-        clickUsername.sendKeys("tomsmith1");
-        WebElement clickPassword = driver.findElement(By.xpath("//input[@name='password']"));
-        clickPassword.sendKeys("SuperSecretPassword!");
-        WebElement clickLogin = driver.findElement(By.xpath("//button[@type='submit']"));
-        clickLogin.click();
-        WebElement verify = driver.findElement(By.linkText("Your username is invalid!"));
+        sendKeysToElement(By.xpath("//input[@name='username']"),"tomsmith");
+      //  WebElement clickUsername = driver.findElement(By.xpath("//input[@name='username']"));
+        //clickUsername.sendKeys("tomsmith1");
+        sendKeysToElement(By.xpath("//input[@name='password']"),"SuperSecretPasswordd!");
+      //  WebElement clickPassword = driver.findElement(By.xpath("//input[@name='password']"));
+       // clickPassword.sendKeys("SuperSecretPassword!");
+        clickOnElement(By.xpath("//button[@type='submit']"));
+      //  WebElement clickLogin = driver.findElement(By.xpath("//button[@type='submit']"));
+        //clickLogin.click();
+        String actualMess = getTextFromElement(By.xpath("//div[@id='flash']"));
+        String expectedMess = "Your password is invalid!\n" + "×";
+        Assert.assertEquals(expectedMess, actualMess);
+
+        /*WebElement verify = driver.findElement(By.linkText("Your username is invalid!"));
         String actMess = verify.getText();
         String expMess = "Your username is invalid!";
-        Assert.assertEquals(expMess, actMess);
+        Assert.assertEquals(expMess, actMess); */
 
     }
     //div[@class='flash error'] ---- xpath verify error message username
@@ -69,16 +85,23 @@ public class LoginTest extends BaseTest {
      * Verify the error message “Your password is invalid!”    */
     @Test
     public void verifyThePasswordErrorMessage() {
-        WebElement clickUsername = driver.findElement(By.xpath("//input[@name='username']"));
-        clickUsername.sendKeys("tomsmith");
-        WebElement clickPassword = driver.findElement(By.xpath("//input[@name='password']"));
-        clickPassword.sendKeys("SuperSecretPassword");
-        WebElement clickLogin = driver.findElement(By.xpath("//button[@type='submit']"));
-        clickLogin.click();
-        WebElement verify = driver.findElement(By.xpath("//div[@id='flash']"));
+        sendKeysToElement(By.xpath("//input[@name='username']"),"ttomsmith");
+       // WebElement clickUsername = driver.findElement(By.xpath("//input[@name='username']"));
+       // clickUsername.sendKeys("tomsmith");
+        sendKeysToElement(By.xpath("//input[@name='password']"),"SuperSecretPassword!");
+      //  WebElement clickPassword = driver.findElement(By.xpath("//input[@name='password']"));
+        //clickPassword.sendKeys("SuperSecretPassword");
+        clickOnElement(By.xpath("//button[@type='submit']"));
+      //  WebElement clickLogin = driver.findElement(By.xpath("//button[@type='submit']"));
+        //clickLogin.click();
+        String actualMess = getTextFromElement(By.xpath("//div[@id='flash']"));
+        String expectedMess = "Your username is invalid!\n" +
+                "×";
+        Assert.assertEquals(expectedMess, actualMess);
+       /* WebElement verify = driver.findElement(By.xpath("//div[@id='flash']"));
         String actMess = verify.getText();
         String expMess = "Your username is invalid";
-        Assert.assertEquals(expMess, actMess);
+        Assert.assertEquals(expMess, actMess);  */
         //div[@id='flash']   ---- xpath of verify error message of password
     }
 }
